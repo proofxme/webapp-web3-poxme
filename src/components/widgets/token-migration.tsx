7c4f59d7c0cb05ee6ecff0c1fed6bdf3ae47d435
Jorge de los Santos <tebayoso@gmail.com>
2024-01-07 09:27:26 -0300

    Add asset button solution is more elegant

--- src/components/widgets/token-migration.tsx
+++ src/components/widgets/token-migration.tsx
@@ -164,69 +164,12 @@ export default function TokenMigration() {
 
   const {blocksPending, deposited, claimed, pending, available} = calculateAmounts()
 
-  let client: any;
-
-  // @ts-ignore
-  if (window && window.ethereum) {
-    // @ts-ignore
-    const client = createWalletClient({chain: mainnet, transport: custom(window.ethereum),})
-  }
-
-  return <div>
-    <Card className="mb-4 my-3">
-      <CardHeader>
-        <h3 className="text-lg font-semibold">Deposit <span style={{color: 'blue'}}>$EULER</span></h3>
-      </CardHeader>
-      <CardContent>
-        <p className="text-gray-500 pb-6">Deposit <span style={{color: 'blue'}}>$EULER</span> to claim
-          the new token</p>
-        <p className="text-gray-500">You have <strong
-          style={{color: 'black'}}>{Number(eulerBalance?.formatted).toFixed(2)}</strong> <span
-          style={{color: 'blue'}}>$EULER</span> to
-          migrate</p>
-
-        {oldTokenAllowance as number > 0 && <Button
-          className="mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 ml-2 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
-          onClick={(e) => {
-            const visibleValue = BigInt(Math.trunc(Number(eulerBalance?.formatted)))
-            const value = BigInt(Number(visibleValue) * 10 ** 18)
-            setVisibleAmount(visibleValue)
-            setDepositAmount(value);
-          }
-          }>Max
-          Tokens</Button>}
+  const addAsset = () => {
+    if (address) {
+      const client = createWalletClient({chain: mainnet, transport: custom(window.ethereum),})
+      return (
         <div className="space-x-3 mb-6">
-          {oldTokenAllowance as number > 0 ? <div className="flex justify-between pt-3">
-            <Input
-              placeholder="Amount"
-              type="number"
-              value={visibleAmount.toString()}
-              onChange={(e) => {
-                const value = BigInt(Number(e.target.value) * 10 ** 18)
-                const visibleValue = BigInt(Number(e.target.value))
-                setVisibleAmount(visibleValue)
-                setDepositAmount(value);
-              }}/>
-            <Button disabled={depositAmount === BigInt(0) || !isMigrationActive}
-                    className="pt-3 mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 ml-2 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
-                    onClick={() => depositTokens?.()}>Deposit</Button>
-          </div> : <Button
-            className="mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
-            onClick={() => approveOldToken?.()}
-          >
-            Approve Contract
-          </Button>
-          }
-        </div>
-      </CardContent>
-    </Card>
-    <Card>
-      <CardHeader>
-        <h3 className="text-lg font-semibold">Claim new <strong style={{color: 'purple'}}>$POXME</strong> Tokens</h3>
-      </CardHeader>
-      <CardContent>
-        <div className="space-x-3 mb-6">
-          {address && client && (
+          {address && (
             <Button
               className="bg-gradient-to-r from-purple-900 via-white-500 to-purple-500 hover:from-purple-400 hover:via-white-500 hover:to-purple-500"
               onClick={() => {
@@ -242,45 +185,105 @@ export default function TokenMigration() {
               Add Token to Wallet
             </Button>
           )}
-        </div>
-        <div className="space-x-3 mb-6">
-          <p className="text-gray-500">
-            You have deposited:
-            <span
-              className="font-bold text-gray-900"> {deposited} tokens
+        </div>)
+    } else {
+      return null
+    }
+  }
+
+  return (
+    <div>
+      <Card className="mb-4 my-3">
+        <CardHeader>
+          <h3 className="text-lg font-semibold">Deposit <span style={{color: 'blue'}}>$EULER</span></h3>
+        </CardHeader>
+        <CardContent>
+          <p className="text-gray-500 pb-6">Deposit <span style={{color: 'blue'}}>$EULER</span> to claim
+            the new token</p>
+          <p className="text-gray-500">You have <strong
+            style={{color: 'black'}}>{Number(eulerBalance?.formatted).toFixed(2)}</strong> <span
+            style={{color: 'blue'}}>$EULER</span> to
+            migrate</p>
+
+          {oldTokenAllowance as number > 0 && <Button
+            className="mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 ml-2 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
+            onClick={(e) => {
+              const visibleValue = BigInt(Math.trunc(Number(eulerBalance?.formatted)))
+              const value = BigInt(Number(visibleValue) * 10 ** 18)
+              setVisibleAmount(visibleValue)
+              setDepositAmount(value);
+            }
+            }>Max
+            Tokens</Button>}
+          <div className="space-x-3 mb-6">
+            {oldTokenAllowance as number > 0 ? <div className="flex justify-between pt-3">
+              <Input
+                placeholder="Amount"
+                type="number"
+                value={visibleAmount.toString()}
+                onChange={(e) => {
+                  const value = BigInt(Number(e.target.value) * 10 ** 18)
+                  const visibleValue = BigInt(Number(e.target.value))
+                  setVisibleAmount(visibleValue)
+                  setDepositAmount(value);
+                }}/>
+              <Button disabled={depositAmount === BigInt(0) || !isMigrationActive}
+                      className="pt-3 mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 ml-2 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
+                      onClick={() => depositTokens?.()}>Deposit</Button>
+            </div> : <Button
+              className="mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
+              onClick={() => approveOldToken?.()}
+            >
+              Approve Contract
+            </Button>
+            }
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <h3 className="text-lg font-semibold">Claim new <strong style={{color: 'purple'}}>$POXME</strong> Tokens</h3>
+        </CardHeader>
+        <CardContent>
+          {addAsset()}
+          <div className="space-x-3 mb-6">
+            <p className="text-gray-500">
+              You have deposited:
+              <span
+                className="font-bold text-gray-900"> {deposited} tokens
         </span>
-          </p>
-          <p className="text-gray-500 mt-2">
-            You have claimed:
-            <span
-              className="font-bold text-gray-900"> {claimed} tokens </span>
-          </p>
-          <p className="text-gray-500 mt-2">
-            You can claim:
-            <span
-              className="font-bold text-gray-900"> {pending} tokens </span>
-          </p>
-          <p className="text-gray-500 mt-2">
-            You can claim after:
-            <span
-              className="font-bold text-gray-900"> {blocksPending} blocks</span>
-          </p>
-        </div>
-        <div className="space-x-3 mb-6">
-          {oldTokenAllowance as number > 0 ? <div className="flex justify-center pt-3">
-            <Button disabled={pending === 0 || !isMigrationActive}
-                    className="mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 ml-2 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
-                    onClick={() => claimTokens?.()}>Claim Tokens</Button>
-          </div> : <Button
-            className="mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
-            onClick={() => approveOldToken?.()}
-          >
-            Approve Contract
-          </Button>
-          }
-        </div>
-      </CardContent>
-    </Card>
-  </div>
-    ;
+            </p>
+            <p className="text-gray-500 mt-2">
+              You have claimed:
+              <span
+                className="font-bold text-gray-900"> {claimed} tokens </span>
+            </p>
+            <p className="text-gray-500 mt-2">
+              You can claim:
+              <span
+                className="font-bold text-gray-900"> {pending} tokens </span>
+            </p>
+            <p className="text-gray-500 mt-2">
+              You can claim after:
+              <span
+                className="font-bold text-gray-900"> {blocksPending} blocks</span>
+            </p>
+          </div>
+          <div className="space-x-3 mb-6">
+            {oldTokenAllowance as number > 0 ? <div className="flex justify-center pt-3">
+              <Button disabled={pending === 0 || !isMigrationActive}
+                      className="mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 ml-2 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
+                      onClick={() => claimTokens?.()}>Claim Tokens</Button>
+            </div> : <Button
+              className="mt-4inline-flex items-center rounded-md border border-transparent bg-gray-900 px-2.5 py-0.5 text-xs font-semibold text-gray-50 shadow transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:border-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
+              onClick={() => approveOldToken?.()}
+            >
+              Approve Contract
+            </Button>
+            }
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
