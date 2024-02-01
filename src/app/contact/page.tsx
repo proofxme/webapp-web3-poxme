031478caf79e65a318f902dac24f999ccc586243
Ariel Vincennao <arielvincennao@hotmail.com>
2024-02-01 19:46:55 -0300

    Deleted comment, added whiteline in the end

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import { Card, CardHeader } from "@/components/ui/card"
-//import '../globals.css'
 
 const ContactPage = () => {
   return (
