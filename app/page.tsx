4a8971719a022ad73c38665f45934084ff4e96e5
Ariel Vincennao <arielvincennao@hotmail.com>
2024-02-05 22:48:17 -0300

    Added first version footer. Worked in content distribution.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { JSX, SVGProps } from "react"
 import { TopNavBrowser } from "@/components/ui/navigations/top-nav-browser";
 import { HeroCustomers } from "@/components/widgets/hero-component";
 import { HeroPerformance } from "@/components/widgets/hero-performance";
+import Footer from "@/components/footer";
 
 
 export default function Component() {
@@ -192,17 +193,7 @@ export default function Component() {
         {false && <HeroCustomers/>}
         {false && <HeroPerformance/>}
       </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Terms of Service
-          </Link>
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Privacy
-          </Link>
-        </nav>
-      </footer>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white p-12 flex flex-col items-center justify-center">
+            <div className="container mx-auto grid grid-cols-1 md:grid-cols-5 gap-5 text-center mt-12">
+                <div className="mb-4 md:mb-0">
+                    <h3 className="text-lg font-bold mb-2">Pox Me Icon</h3>
+                </div>
+                <div className="mb-4 md:mb-0">
+                    <h3 className="text-lg font-bold mb-2">Information</h3>
+                    <ul>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Product
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="mb-4 md:mb-0">
+                    <h3 className="text-lg font-bold mb-2">Contact</h3>
+                    <ul>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Link 1
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Link 2
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="mb-4 md:mb-0">
+                    <h3 className="text-lg font-bold mb-2">News</h3>
+                    <ul>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Link 1
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Link 2
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Link 3
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="mb-4 md:mb-0">
+                    <h3 className="text-lg font-bold mb-2">Follow Us</h3>
+                    <ul>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Link 1
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Link 2
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="text-md hover:underline underline-offset-4" href="#">
+                                Link 3
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div className="w-full text-center mt-12 pt-8">
+                    <Link className="text-xs hover:underline underline-offset-4" href="#">
+                        Terms of Service
+                    </Link>
+                    <span className="mx-2 text-xs text-gray-500 dark:text-gray-400">|</span>
+                    <Link className="text-xs hover:underline underline-offset-4" href="#">
+                        Privacy
+                    </Link>
+                </div>
+                <div className="w-full text-center mt-4">
+                    <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
