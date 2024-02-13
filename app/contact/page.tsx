e4a68b88082a2439abd56a487214a238e86030fc
d4rm5 <dantemartinez06@gmail.com>
2024-02-13 11:05:27 -0300

    Testing .metadata fron external file  in /contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,42 +1,9 @@
 "use client";
 
-import type { Metadata } from "next";
+import { metadata } from "./metadata";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader } from "@/components/ui/card";
 import SocialIconsSection from "@/components/socialIconsSection";
-
-const metadata: Metadata = {
-  title: "Proof of X | Contact",
-  description:
-    "Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.",
-  openGraph: {
-    title: "Proof of X | Contact",
-    description:
-      "Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.",
-
-    url: "https://pox.me/contact",
-    siteName: "Proof of X",
-    images: [
-      {
-        url: "/images/home-hero.jpg",
-        width: 1800,
-        height: 1600,
-        alt: "A cool fingerprint image",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Proof of X | Contact",
-    description:
-      "Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.",
-
-    creator: "@proofxme",
-    images: ["/images/home-hero.jpg"],
-  },
-};
 
 const ContactPage = () => {
   return (
