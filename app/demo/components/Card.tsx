c5a5bb2d3132c2662b39e2aece2f522bf295d3a9
Danilo Donado <danilodonadoa@gmail.com>
2024-02-22 12:35:42 -0400

    fixed white lines and indentation in files, wrapped images in separate folders, alt attribute added to images, added interfaces for props, removed redundant functions and unnecessary files

--- app/demo/components/Card.tsx
+++ app/demo/components/Card.tsx
@@ -1,20 +1,24 @@
-import { CollageData } from "./Data";
+interface CardProps {
+  id: number,
+  src: string,
+  title: string,
+  text: string,
+  alt: string
+}
 
-export default function Card(props) {
+export default function Card(props: CardProps) {
+  const { id, src, title, text, alt } = props;
 
-    const { id, src, title, text } = props;
-
-    return (
-        <div className="my-7 mx-7">
-            <div className="max-w-sm bg-white text-black font-semibold text-sm border rounded-3xl flex flex-col items-center gap-1 transition ease-in-out delay-90 hover:scale-110">
-                <div>
-                    <img className="max-w-xs border rounded-3xl" key={id} src={src} />
-                </div>
-                <div>
-                    {title}
-                </div>
-            </div>
+  return (
+    <div className="my-7 mx-7">
+      <div className="max-w-sm bg-white text-black font-semibold text-sm border rounded-3xl flex flex-col items-center gap-1 transition ease-in-out delay-90 hover:scale-110">
+        <div>
+          <img className="max-w-xs border rounded-3xl" key={id} src={src} alt={alt}/>
         </div>
-
-    )
+        <div>
+          {title}
+        </div>
+      </div>
+    </div>
+  )
 }
--- app/demo/demo.tsx
+++ app/demo/demo.tsx
@@ -1,19 +1,11 @@
 'use client';
-import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import InfoBox from "./components/InfoBox";
 import Card from "./components/Card";
-import {affiliateData, membershipData, CollageData} from "./components/Data"
+import {  affiliateData, membershipData, CollageData  } from "./components/Data"
 
 export default function Demo() {
-
-  const card = CollageData.map((val) => {
-    return (
-      <Card id={val.id} src={val.src} title={val.title} text={val.text} />
-    )
-  })
-
   return (
     <div>
       <main className="bg-zinc-100">
@@ -27,15 +19,23 @@ export default function Demo() {
               <div className="text-xl my-7 ml-14 font-medium max-w-xl">Embrace the evolution of digital identity: integrate the steadfast reliability of email with the dynamic possibilities of Web3.</div>
             </div>
             <div className="self-center flex justify-center flex-wrap">
-              {card}
+              {
+                CollageData.map((val) => {
+                  return (
+                    <Card id={val.id} src={val.src} title={val.title} text={val.text} alt={val.alt}/>
+                  )
+                })
+              }
             </div>
           </div>
         </section>
         <section>
           <div className="bg-slate-800 min-h-svh flex flex-col items-center">
-            <div className="text-4xl font-semibold text-white my-20">Explore the POX.ME identity management system</div>
-            <InfoBox title={"Affiliate NFTs"} text={affiliateData} src="./images/Affiliate" />
-            <InfoBox title={"Membership NFTs"} text={membershipData} src="./images/Membership" />
+            <div className="text-4xl font-semibold text-white my-20">
+              Explore the POX.ME identity management system
+            </div>
+            <InfoBox title={"Affiliate NFTs"} text={affiliateData} src="./images/nft-images/affiliate" alt="Affiliate nft" />
+            <InfoBox title={"Membership NFTs"} text={membershipData} src="./images/nft-images/membership" alt="Membership nft" />
           </div>
         </section>
       </main>
@@ -69,7 +69,7 @@ export default function Demo() {
           </div>
         </div>
         <div className="bg-slate-900 text-slate-400 flex justify-center text-xs">
-            © Proof of X. All right reserved.
+          © Proof of X. All right reserved.
         </div>
       </footer>
     </div>
