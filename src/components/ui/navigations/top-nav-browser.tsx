4a20e7ab104318401706471b8322eb901ddaceb2
d4rm5 <dantemartinez06@gmail.com>
2024-02-27 22:51:51 -0300

    changed navigation

--- src/components/ui/navigations/top-nav-browser.tsx
+++ src/components/ui/navigations/top-nav-browser.tsx
@@ -125,10 +125,10 @@ export function TopNavBrowser() {
                 <NavigationMenuLink asChild>
                   <Link
                     className="group grid h-auto w-full items-center justify-start gap-1 rounded-md bg-white p-4 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-                    href="/features/web3"
+                    href="/features/investors"
                   >
                     <div className="text-sm font-medium leading-none group-hover:underline">
-                      Web3
+                      investors
                     </div>
                     <div className="line-clamp-2 text-sm leading-snug text-gray-500 dark:text-gray-400">
                       Protect your communications using the best of blockchain.
@@ -206,30 +206,53 @@ export function TopNavBrowser() {
               </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="/web3/investors"
-            >
-              Invest
-            </Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="/web3/membership"
-            >
-              Membership
-            </Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="/web3/migration"
-            >
-              Migration
-            </Link>
-          </NavigationMenuLink>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>For investors</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <div className="grid w-[400px] p-2">
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="group grid h-auto w-full items-center justify-start gap-1 rounded-md bg-white p-4 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+                    href="/investors/token"
+                  >
+                    <div className="text-sm font-medium leading-none group-hover:underline">
+                      $POXME token
+                    </div>
+                    <div className="line-clamp-2 text-sm leading-snug text-gray-500 dark:text-gray-400">
+                      All about the $POXME token.
+                    </div>
+                  </Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="group grid h-auto w-full items-center justify-start gap-1 rounded-md bg-white p-4 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+                    href="/investors/membership"
+                  >
+                    <div className="text-sm font-medium leading-none group-hover:underline">
+                      Membership
+                    </div>
+                    <div className="line-clamp-2 text-sm leading-snug text-gray-500 dark:text-gray-400">
+                      All about memberships.
+                    </div>
+                  </Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="group grid h-auto w-full items-center justify-start gap-1 rounded-md bg-white p-4 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+                    href="/investors/migration"
+                  >
+                    <div className="text-sm font-medium leading-none group-hover:underline">
+                      Migration
+                    </div>
+                    <div className="line-clamp-2 text-sm leading-snug text-gray-500 dark:text-gray-400">
+                      Migrate $EULER to $POXME and get a membership.
+                    </div>
+                  </Link>
+                </NavigationMenuLink>
+              </div>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+
           <NavigationMenuLink asChild>
             <Link
               className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
@@ -283,7 +306,29 @@ export function TopNavBrowser() {
                   <DropdownMenuItem disabled>
                     Security & Compliance
                   </DropdownMenuItem>
-                  <DropdownMenuItem disabled>Web3</DropdownMenuItem>
+                  <DropdownMenuItem disabled>investors</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    className="flex items-center justify-start text-lg font-semibold py-0 px-0"
+                    variant="link"
+                  >
+                    For investors
+                    <ChevronDownIcon aria-hidden="true" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="start">
+                  <DropdownMenuItem asChild>
+                    <Link href="/investors/token">$POXME Token</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/investors/membership">Memberships</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link href="/investors/migration">Migration</Link>
+                  </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
               <Button
@@ -317,36 +362,6 @@ export function TopNavBrowser() {
                   <DropdownMenuItem disabled>Help Center</DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
-              <Link
-                className="flex h-auto items-center justify-start gap-1 text-lg font-semibold"
-                href="/web3/investors"
-              >
-                Invest
-              </Link>
-              <Link
-                className="flex h-auto items-center justify-start gap-1 text-lg font-semibold"
-                href="/web3/membership"
-              >
-                Membership
-              </Link>
-              <Link
-                className="flex h-auto items-center justify-start gap-1 text-lg font-semibold"
-                href="/web3/migration"
-              >
-                Migration
-              </Link>
-              <Link
-                className="flex h-auto items-center justify-start gap-1 text-lg font-semibold"
-                href="/roadmap"
-              >
-                Roadmap
-              </Link>
-              <Link
-                className="flex h-auto items-center justify-start gap-1 text-lg font-semibold"
-                href="/contact"
-              >
-                Contact
-              </Link>
             </div>
           </SheetContent>
         </Sheet>
