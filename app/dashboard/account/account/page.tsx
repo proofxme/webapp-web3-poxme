ae21d6f954253a633f42e91dfa57cee452f03edc
Jorge de los Santos <tebayoso@gmail.com>
2024-03-02 19:24:18 -0300

    Added account Settings, Notifications Settings, and Appearance settings

---
+++ app/dashboard/account/account/page.tsx
@@ -0,0 +1,20 @@
+'use server';
+
+import { Separator } from "@/components/ui/separator"
+import { AccountForm } from "@/components/ui/account/account-form";
+
+export default async function SettingsAccountPage() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Account</h3>
+        <p className="text-sm text-muted-foreground">
+          Update your account settings. Set your preferred language and
+          timezone.
+        </p>
+      </div>
+      <Separator/>
+      <AccountForm/>
+    </div>
+  )
+}
---
+++ app/dashboard/account/page.tsx
@@ -0,0 +1,19 @@
+'use server';
+
+import { Separator } from "@/components/ui/separator"
+import { ProfileForm } from "@/components/ui/account/profile-form";
+
+export default async function AccountSettings() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Profile</h3>
+        <p className="text-sm text-muted-foreground">
+          This is how others will see you on the site.
+        </p>
+      </div>
+      <Separator/>
+      <ProfileForm/>
+    </div>
+  )
+}
