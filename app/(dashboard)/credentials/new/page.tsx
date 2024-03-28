d9c99af0a4c6e4bebd84764b2d1ca44b5c6b203b
Jorge de los Santos <tebayoso@gmail.com>
2024-03-28 14:06:36 -0300

    Create credential working

---
+++ app/(dashboard)/credentials/new/page.tsx
@@ -0,0 +1,29 @@
+'use server';
+
+import React from "react";
+import AddEmailComponent from "app/(dashboard)/credentials/new/add-email-component";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createCredential } from "app/api/credentials/create-credentials";
+
+export default async function NewCredential() {
+  const handleCredential = async (data: any) => {
+    'use server';
+    await createCredential(data);
+    revalidatePath('/credentials');
+    redirect('/credentials');
+  }
+
+  return (
+    <div className="flex w-full min-h-screen items-start py-4 gap-4 flex-col">
+      <div className="container mx-auto px-4">
+        <div className="grid gap-4">
+          <div className="flex items-center gap-4">
+            <h1 className="font-semibold text-lg md:text-2xl">New Credential</h1>
+          </div>
+          <AddEmailComponent handleCredential={handleCredential}/>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/api/credentials/route.ts
+++ app/api/credentials/route.ts
@@ -4,7 +4,7 @@ import { logtoClient } from "@/lib/logto-edge";
 export const runtime = 'edge';
 
 export async function GET(request: NextRequest) {
-  const {isAuthenticated, scopes, accessToken, userInfo, claims} = await logtoClient.getLogtoContext(request, {
+  const {isAuthenticated, scopes, accessToken} = await logtoClient.getLogtoContext(request, {
     getAccessToken: true,
     resource: 'https://api.pox.me'
   });
@@ -19,7 +19,7 @@ export async function GET(request: NextRequest) {
     });
   }
 
-  const credentials = await fetch('https://api.pox.me/credentials', {
+  const response = await fetch('https://api.pox.me/credentials', {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
@@ -27,7 +27,40 @@ export async function GET(request: NextRequest) {
 
   return new Response(
     JSON.stringify({
-      data: credentials,
-    })
+      data: response,
+    }), {status: 200}
+  );
+}
+
+export async function POST(request: NextRequest) {
+  const {isAuthenticated, scopes, accessToken} = await logtoClient.getLogtoContext(request, {
+    getAccessToken: true,
+    resource: 'https://api.pox.me'
+  });
+
+  if (!isAuthenticated) {
+    return new Response(JSON.stringify({message: 'Unauthorized api'}), {status: 401});
+  }
+
+  if (!scopes?.includes('write:credential')) {
+    return new Response(JSON.stringify({message: 'Access denied to route, requires write:credential scope.'}), {
+      status: 403,
+    });
+  }
+
+  const json = await request.json();
+
+  const response = await fetch('https://api.pox.me/credentials', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    method: 'POST',
+    body: JSON.stringify(json),
+  }).then((res) => res.json());
+
+  return new Response(
+    JSON.stringify({
+      data: response,
+    }), {status: 200}
   );
 }
