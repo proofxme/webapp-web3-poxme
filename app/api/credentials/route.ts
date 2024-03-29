caefd27c23942b0e6f5f54ea81debfa114f97f26
Jorge de los Santos <tebayoso@gmail.com>
2024-03-29 09:13:55 -0300

    Delete Credentials initial commit

--- app/api/credentials/route.ts
+++ app/api/credentials/route.ts
@@ -64,3 +64,38 @@ export async function POST(request: NextRequest) {
     }), {status: 200}
   );
 }
+
+export async function DELETE(request: NextRequest) {
+  const {isAuthenticated, scopes, accessToken} = await logtoClient.getLogtoContext(request, {
+    getAccessToken: true,
+    resource: 'https://api.pox.me'
+  });
+
+  if (!isAuthenticated) {
+    return new Response(JSON.stringify({message: 'Unauthorized api'}), {status: 401});
+  }
+
+  if (!scopes?.includes('write:credential')) {
+    return new Response(JSON.stringify({message: 'Access denied to route, requires write:credential scope.'}), {
+      status: 403,
+    });
+  }
+
+  const json = await request.json();
+
+  const {provider} = json;
+
+  const response = await fetch(`https://api.pox.me/credentials/${provider}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    method: 'DELETE',
+    body: JSON.stringify(json),
+  }).then((res) => res.json());
+
+  return new Response(
+    JSON.stringify({
+      data: response,
+    }), {status: 200}
+  );
+}
