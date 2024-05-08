a878897a774bd83cc9f3b05ed7fe4e35dd4b7f69
Jorge de los Santos <tebayoso@gmail.com>
2024-05-08 18:47:31 -0300

    Fixed Build

--- app/(id)/[id]/layout.tsx
+++ app/(id)/[id]/layout.tsx
@@ -2,7 +2,7 @@
 
 import "@rainbow-me/rainbowkit/styles.css";
 
-export default function Layout({children}: { children: React.ReactNode; metadata: Metadata }) {
+export default function Layout({children}: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen text-[#000000]">
       {children}
