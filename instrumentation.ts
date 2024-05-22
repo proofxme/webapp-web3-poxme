da082fcef984d656d1115bdd0fde0598fd782c02
Jorge de los Santos <tebayoso@gmail.com>
2024-05-22 11:52:24 -0300

    Updated sentry config to use instrument

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,9 +1,31 @@
+import * as Sentry from "@sentry/nextjs";
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
+    Sentry.init({
+      dsn: "https://7abeb601ecc9eb04267583da31f014f4@o4506530285289472.ingest.us.sentry.io/4506530286141440",
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+    });
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+    Sentry.init({
+      dsn: "https://7abeb601ecc9eb04267583da31f014f4@o4506530285289472.ingest.us.sentry.io/4506530286141440",
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+
+      // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+      // spotlight: process.env.NODE_ENV === 'development',
+
+    });
   }
 }
