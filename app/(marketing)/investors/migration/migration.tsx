aa8ac0f9e35504c47bd11a375725d936bf536025
Jorge de los Santos <tebayoso@gmail.com>
2024-06-06 15:26:50 -0300

    Migration period is over

--- app/(marketing)/investors/migration/migration.tsx
+++ app/(marketing)/investors/migration/migration.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Questions from "@/components/widgets/questions";
-import StakingDetails from "@/components/widgets/staking-details";
 import { Button } from "@/components/ui/button";
 import MigrationDetails from "@/components/widgets/migration-details";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -11,137 +9,32 @@ export default function Migration() {
     <main className="flex min-h-screen flex-col h-auto bg-gray-50 px-3">
       <div className="flex flex-col items-center justify-center w-full h-full lg:px-12">
         <div className=" text-center w-full">
-          <h2 className="text-5xl text-center font-bold tracking-tighter mt-9 mb-2">
-            Migration Center
-          </h2>
-          <p
-            className="mx-auto text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400 p-6">
-            During the migration period, you can deposit{" "}
-            <i style={{color: "blue"}} className={"" + "font-bold"}>
-              $EULER{" "}
-            </i>
-            to get the new token and claim your membership NFTs.
-          </p>
-          <p
-            className="mx-auto text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400 md:w-10/12 mb-12">
-            The migration process is a unique opportunity to continue
-            supporting <strong>@tebayoso</strong> during the Proof of X
-            development, while maintaining the value you deposited and have an
-            opportunity to claim back the investment.
-          </p>
-          <Button
-            className="bg-gradient-to-r from-blue-900 via-white-500 to-blue-500 hover:from-blue-400 hover:via-white-500 hover:to-blue-500"
-            onClick={() => window.open("https://t.me/proofxme", "_blank")}
+          <div
+            className="bg-red-100 border-l-4 border-orange-500 text-orange-700 p-4 mt-8"
+            role="alert"
           >
-            <svg
-              className="h-5 w-5 mr-5"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
+            <p className="font-bold">Migration Period is over</p>
+            <p className="text-sm">
+              The migration process is over, and the new token is live. You cannot migrate any more tokens to the new
+              token, but you can acquire <i style={{color: "blue"}} className={"" + "font-bold"}>
+              $POXME{" "}
+            </i> at Uniswap on this link: <br/>
+            </p>
+            <Button
+              className="bg-gradient-to-r from-blue-900 via-white-500 to-blue-500 hover:from-blue-400 hover:via-white-500 hover:to-blue-500 mt-4"
+              onClick={() => window.open("https://app.uniswap.org/swap?outputCurrency=0xb469783b6b3615180da05571beec716b639cbe85", "_blank")}
             >
-              <path
-                id="telegram-1"
-                d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z"
-              />
-            </svg>
-            Join our telegram{" "}
-          </Button>
-        </div>
-        <div
-          className="bg-red-100 border-l-4 border-orange-500 text-orange-700 p-4 mt-8"
-          role="alert"
-        >
-          <p className="font-bold">Irreversible Actions</p>
-          <p className="text-sm">
-            Please, read the FAQ section, or contact us in the official
-            telegram channel if you have doubts.
-            <br/>
-            This is an <b>irreversible migration process.</b>
-          </p>
+              Buy at Uniswap{" "}
+            </Button>
+          </div>
         </div>
         <div className="flex items-center justify-center w-full h-full pt-12">
           <ConnectButton/>
         </div>
         <div className="pt-12 px-2">
-          <ul
-            className="relative m-0 w-full list-none p-0 transition-[height] duration-200 ease-in-out mr-10"
-            data-te-stepper-init="true"
-            data-te-stepper-type="vertical"
-          >
-            <li
-              data-te-stepper-step-ref="true"
-              className="relative h-fit after:absolute after:left-[3.0rem] after:top-[3.5rem] after:mt-px after:h-[calc(100%-2.45rem)] sm:after:w-px after:bg-[#4e77d4] after:content-[''] dark:after:bg-neutral-600"
-            >
-              <div
-                data-te-stepper-head-ref=""
-                className="flex items-center p-6 leading-[1.3rem] no-underline after:bg-[#e0e0e0] after:content-[''] focus:outline-none dark:after:bg-neutral-600"
-              >
-                  <span
-                    data-te-stepper-head-icon-ref="true"
-                    className="mr-6 flex h-[3.2rem] w-[3.2rem] items-center justify-center rounded-full bg-[#4e77d4] lg:text-lg font-medium text-[#ffffff]"
-                  >
-                    1
-                  </span>
-                <span
-                  data-te-stepper-head-text-ref=""
-                  className="text-xl after:absolute after:flex after:text-[0.8rem] after:content-[data-content] dark:text-neutral-300 font-semibold md:text-3xl"
-                >
-                    Withdraw the tokens from the staking
-                  </span>
-              </div>
-              <div className="w-full">
-                <StakingDetails/>
-              </div>
-            </li>
-            <li
-              data-te-stepper-step-ref="true"
-              className="relative h-fit after:absolute after:left-[3.0rem] after:top-[3.5rem] after:mt-px after:h-[calc(100%-2.45rem)] sm:after:w-px after:bg-[#4e77d4] after:content-[''] dark:after:bg-neutral-600"
-            >
-              <div
-                data-te-stepper-head-ref="true"
-                className="flex items-center p-6 leading-[1.3rem] no-underline after:bg-[#e0e0e0] after:content-[''] focus:outline-none dark:after:bg-neutral-600"
-              >
-                  <span
-                    data-te-stepper-head-icon-ref="true"
-                    className="mr-6 flex h-[3.2rem] w-[3.2rem] items-center justify-center rounded-full bg-[#4e77d4] text-lg font-medium text-[#ffffff]"
-                  >
-                    2
-                  </span>
-                <span
-                  data-te-stepper-head-text-ref="true"
-                  className="after:absolute after:flex after:text-[0.8rem] after:content-[data-content] dark:text-neutral-300 font-semibold md:text-3xl text-xl"
-                >
-                    Migrate the tokens to the new Protocol
-                  </span>
-              </div>
-              <div className="w-full">
-                <MigrationDetails/>
-              </div>
-            </li>
-            <li data-te-stepper-step-ref="true" className="relative h-fit">
-              <div
-                data-te-stepper-head-ref="true"
-                className="flex items-center p-6 leading-[1.3rem] no-underline after:bg-[#e0e0e0] after:content-[''] focus:outline-none dark:after:bg-neutral-600"
-              >
-                  <span
-                    data-te-stepper-head-icon-ref="true"
-                    className="mr-6 flex h-[3.2rem] w-[3.2rem] items-center justify-center rounded-full bg-[#4e77d4] text-lg font-medium text-[#ffffff]"
-                  >
-                    3
-                  </span>
-                <span
-                  data-te-stepper-head-text-ref="true"
-                  className="text-3xl after:absolute after:flex after:text-[0.8rem] after:content-[data-content] dark:text-neutral-300 font-semibold md:text-3xl text-2xl"
-                >
-                    You are done!
-                  </span>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className="mt-12 pt-12 px-2 lg:w-3/4">
-          <Questions/>
+          <div className="w-full">
+            <MigrationDetails/>
+          </div>
         </div>
       </div>
     </main>
--- src/components/widgets/migration-details.tsx
+++ src/components/widgets/migration-details.tsx
@@ -1,63 +1,16 @@
 "use client";
 
-import TokenMigration from "@/components/widgets/token-migration";
 import AffiliateMigration from "@/components/widgets/affiliate-migration";
 import MembershipMigration from "@/components/widgets/membership-migration";
 import { useAccount } from "wagmi";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Card, CardContent, CardHeader } from "../ui/card";
-import Image from "next/image";
 
 export default function MigrationDetails() {
   const {address} = useAccount();
   return (
     <section key="2" className="w-full lg:w-3/4 lg:m-auto">
       <div className="grid items-center justify-center gap-4 text-center md:px-6 lg:gap-10">
-        {!address && (
-          <Card className="mx-auto bg-white rounded-xl shadow-md overflow-hidden md:w-[60rem]">
-            <CardHeader className="flex items-center space-x-3">
-              <Image
-                alt="Logo"
-                className="h-12 w-12"
-                height="50"
-                src="/tokens/poxme.jpg"
-                style={{
-                  aspectRatio: "50/50",
-                  objectFit: "cover",
-                  // make the image round
-                  borderRadius: "9999px",
-                }}
-                width="50"
-              />
-            </CardHeader>
-            <CardContent>
-              <>
-                <div className="text-red-700 bg-red-100   px-4 py-3 rounded rounded-base relative mt-6">
-                  Please Connect your wallet to continue
-                </div>
-                <div className="flex justify-center mt-3">
-                  <ConnectButton/>
-                </div>
-              </>
-            </CardContent>
-          </Card>
-        )}
         {address && (
           <>
-            {/* Alert Div (Remains unchanged as it meets the requirements) */}
-            <div className="bg-blue-100 border border-blue-100 text-black px-4 py-3 rounded relative mt-6" role="alert">
-              <h2 className="font-bold text-center">
-                The minimum amount to migrate is 4000 <strong style={{color: "blue"}}>$EULER</strong> tokens
-              </h2>
-              <span className="block sm:inline">The migration represents a procedure designed to benefit all initial supporters of the protocol. Should you have been present from the outset yet were unable to acquire the minimum requirement, please direct message <span
-                className="font-bold">@tebayoso</span> on Telegram.</span>
-            </div>
-
-            {/* TokenMigration Full Width */}
-            <div className="grid grid-cols-1 gap-4 mt-6">
-              {address && <TokenMigration/>}
-            </div>
-
             {/* MembershipMigration and AffiliateMigration */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
               {/* MembershipMigration Half Width on Desktop, Full Width on Mobile */}
